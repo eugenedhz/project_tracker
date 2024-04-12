188a886bca27420f6e19aec0e8a1f973d2f25069
tensho <toropkibn@gmail.com>
2024-04-12 22:44:40 +0300

    revert(tasks.models): remove unneccesary __str__ method"
    This reverts commit 2d561355f965d7940e99f3e60ac9219c9f1a40be.

--- project_tracker/tasks/models.py
+++ project_tracker/tasks/models.py
@@ -7,6 +7,9 @@ class Project(models.Model):
 	description = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return self.name
+
 
 class Task(models.Model):
 	name = models.CharField(max_length=100)
