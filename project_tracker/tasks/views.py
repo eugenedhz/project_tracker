cb3c91d62bf7ea0f9c6f8239ffe37ab4ed0a3a04
tensho <toropkibn@gmail.com>
2024-04-13 00:34:01 +0300

    style(tasks.views): change double quotation marks to single

--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 def index(request):
 	another_page_url = reverse('tasks:another_page')
-	html = f"<h1>Страница приложения tasks</h1><a href='{another_page_url}'>Перейти на другую страницу</a>"
+	html = f'<h1>Страница приложения tasks</h1><a href='{another_page_url}'>Перейти на другую страницу</a>'
 
 	return HttpResponse(html)
 
 
 def another_page(request):
-	return HttpResponse("Это другая страница приложения tasks.")
+	return HttpResponse('Это другая страница приложения tasks.')
