eef0de9bf67834d1d6ea40098f64c4ba3578162a
tensho <toropkibn@gmail.com>
2024-04-12 21:36:38 +0300

    fix(quality_control): wrong config import in core settings

--- project_tracker/project_tracker/settings.py
+++ project_tracker/project_tracker/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
-    'tasks.apps.QualityControlConfig',
+    'quality_control.apps.QualityControlConfig',
 ]
 
 MIDDLEWARE = [
