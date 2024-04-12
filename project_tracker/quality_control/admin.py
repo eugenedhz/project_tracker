55d0fa6782c8482019e503f90a139fec01ed2ce1
tensho <toropkibn@gmail.com>
2024-04-13 00:03:27 +0300

    feat(quality_control): add models to admin panel

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from django.contrib.admin import actions
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import BugReport, FeatureRequest
+
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+	list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+	readonly_fields = ('created_at', 'updated_at')
+	list_filters = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+	search_fields = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+
+	fieldsets = (
+		(None, {
+			'fields': ('title', 'description', 'status', 'priority')
+		}),
+		('Assign to', {
+			'fields': ('project', 'task'),
+			'classes': ('collapse',)
+		}),
+		('Dates', {
+			'fields': ('created_at', 'updated_at'),
+			'classes': ('collapse',)
+		}),
+	)
+
+	actions = ['change_status_to_new', 'change_status_to_in_progress', 'change_status_to_completed']
+
+
+	def change_status_to_new(self, request, queryset):
+		queryset.update(status='New')
+
+
+	def change_status_to_in_progress(self, request, queryset):
+		queryset.update(status='In_progress')
+
+
+	def change_status_to_completed(self, request, queryset):
+		queryset.update(status='Completed')
+
+
+	change_status_to_new.short_description = _('Change status to New')
+	change_status_to_in_progress.short_description = _('Change status to In Progress')
+	change_status_to_completed.short_description = _('Change status to Completed')
+
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+	list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+	readonly_fields = ('created_at', 'updated_at')
+	list_filters = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+	search_fields = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+
+	fieldsets = (
+		(None, {
+			'fields': ('title', 'description', 'status', 'priority')
+		}),
+		('Assign to', {
+			'fields': ('project', 'task'),
+			'classes': ('collapse',)
+		}),
+		('Dates', {
+			'fields': ('created_at', 'updated_at'),
+			'classes': ('collapse',)
+		}),
+	)
--- project_tracker/tasks/admin.py
+++ project_tracker/tasks/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 
 from .models import Project, Task
+from quality_control.models import FeatureRequest, BugReport
 
 
 class TaskInline(admin.TabularInline):
 	model = Task
 	extra = 0
 	fields = ('name', 'description', 'assignee', 'status', 'created_at', 'updated_at')
+	readonly_fields = ('created_at', 'updated_at')
+	can_delete = True
+	show_change_link = True
+
+
+class BugReportInline(admin.TabularInline):
+	model = BugReport
+	extra = 0
+	fields = ('title', 'description', 'status', 'priority', 'created_at', 'updated_at')
+	readonly_fields = ('created_at', 'updated_at')
+	can_delete = True
+	show_change_link = True
+
+
+class FeatureRequestInline(admin.TabularInline):
+	model = FeatureRequest
+	extra = 0
+	fields = ('title', 'description', 'status', 'priority', 'created_at', 'updated_at')
 	readonly_fields = ('created_at', 'updated_at')
 	can_delete = True
 	show_change_link = True
@@ -19,7 +38,7 @@ class ProjectAdmin(admin.ModelAdmin):
 	ordering = ('created_at',)
 	date_hierarchy = 'created_at'
 
-	inlines = [TaskInline]
+	inlines = [TaskInline, FeatureRequestInline, BugReportInline]
 
 
 @admin.register(Task)
@@ -29,3 +48,5 @@ class TaskAdmin(admin.ModelAdmin):
 	search_fields = ('name', 'description')
 	list_editable = ('status', 'assignee')
 	readonly_fields = ('created_at', 'updated_at')
+
+	inlines = [FeatureRequestInline, BugReportInline]
