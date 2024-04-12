7cb791e8a083d936b523e54c6781116c9add4089
tensho <toropkibn@gmail.com>
2024-04-12 21:08:51 +0300

    feat(tasks): add database models

--- project_tracker/tasks/models.py
+++ project_tracker/tasks/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+
+class Task(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	project = models.ForeignKey(
+		Project,
+		related_name='tasks',
+		on_delete=models.CASCADE
+	)
+
+	assignee = models.ForeignKey(
+		User,
+		related_name='tasks',
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
+
+	STATUS_CHOICES = (
+		('New', 'Новая'),
+		('In_progress', 'В работе'),
+		('Completed', 'Завершена')
+	)
+	status = models.CharField(
+		max_length=50,
+		choices=STATUS_CHOICES,
+		default='New'
+	)
