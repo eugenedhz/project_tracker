bf44c18735c1ed027d7bcbdb2291c1ab5bfc6085
tensho <toropkibn@gmail.com>
2024-04-13 22:18:29 +0300

    feat(core): add main page index view

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import IndexView
+
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('', IndexView.as_view()),
 	path('', include('tasks.urls')),
 	path('', include('quality_control.urls')),
 ]
