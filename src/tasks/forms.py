96c56511b6d7af0951849bdc1f3bdfd068a9d8aa
tensho <toropkibn@gmail.com>
2024-04-13 19:01:10 +0300

    feat(tasks): add forms

---
+++ src/tasks/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms import (
+    ModelForm, CharField, EmailField, Form
+)
+
+from .models import Project, Task
+
+
+class FeedbackForm(Form):
+    name = CharField(label='Ваше имя', max_length=100)
+    email = EmailField(label='Электронная почта')
+    message = CharField(widget=forms.Textarea, label='Сообщение')
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'description']
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'description', 'status', 'assignee']
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import ListView, DetailView
+from django.core.mail import send_mail
 
 from .models import Project, Task
+from .forms import FeedbackForm, ProjectForm, TaskForm
 
 
 class IndexView(View):
@@ -32,3 +34,51 @@ class TaskDetailView(DetailView):
 		context['project'] = self.object.project
 		
 		return context
+
+
+def feedback_view(request):
+	if request.method == 'POST':
+		form = FeedbackForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name']
+			email = form.cleaned_data['email']
+			message = form.cleaned_data['message']
+
+			recipients = ['info@example.com']
+			recipients.append(email)
+
+			send_mail(subject, message, email, recipients)
+
+			return redirect('/tasks')
+	else:
+		form = FeedbackForm()
+	
+	return render(request, 'tasks/feedback.html', {'form': form})
+
+
+def create_project(request):
+	if request.method == 'POST':
+		form = ProjectForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('tasks:projects_list')
+	else:
+		form = ProjectForm()
+
+	return render(request, 'tasks/project_create.html', {'form': form})
+
+
+def add_task_to_project(request, project_id):
+	project = get_object_or_404(Project, pk=project_id)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			task = form.save(commit=False)
+			task.project = project
+			task.save()
+			return redirect('tasks:project_detail', project_id=project.id)
+	else:
+		form = TaskForm()
+	
+	return render(request, 'tasks/add_task.html', {'form': form, 'project': project})
